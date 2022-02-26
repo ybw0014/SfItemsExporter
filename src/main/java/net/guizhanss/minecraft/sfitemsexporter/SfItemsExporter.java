5c8548fdcfe67cc6d74442aca36c3f1bdc893ae6
ybw0014 <i@guizhanss.com>
2022-02-26 13:32:00 -0500

    Init

---
+++ src/main/java/net/guizhanss/minecraft/sfitemsexporter/SfItemsExporter.java
@@ -0,0 +1,17 @@
+package net.guizhanss.minecraft.sfitemsexporter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SfItemsExporter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
